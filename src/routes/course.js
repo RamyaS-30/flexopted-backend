00d9d687f5e42670f6ac9dbdb7c9d58350cb7f1e
RamyaS-30 <ramyasundar030@gmail.com>
2025-11-18 15:20:44 +0530

    Update course.js

--- src/routes/course.js
+++ src/routes/course.js
@@ -22,7 +22,6 @@ export default (Course, CourseProgress, Enrollment) => {
 
   const upload = multer({ 
   storage,
-  limits: { fileSize: 1 * 1024 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     const ext = path.extname(file.originalname).toLowerCase();
     const allowedExts = ['.mp4', '.avi', '.mov', '.mkv'];
